cf6089798276938cf083369029ba8e87eb9de4fa
adriac02 <adriac02@ucm.es>
2023-04-26 14:35:12 +0200

    Empezando a crear la practica 3

---
+++ Practicas/G02P3/src/main/java/g02/individuals/Individuo.java
@@ -0,0 +1,159 @@
+package g02.individuals;
+
+import java.util.Random;
+
+// TODO: Auto-generated Javadoc
+/**
+ * Individuo.
+ *
+ * @param <T> the generic type
+ */
+public abstract class Individuo<T> {
+
+  /** Cromosoma del individuo. */
+  protected T[] chromosome;
+
+  /** Valor máximo de cada gen. */
+  protected double[] max;
+  
+  /**Valor minimo de cada gen. */
+  protected double[] min;
+  
+  /** Tamaño de cada gen. */
+  protected int[] tamGenes;
+  
+  /** Tamaño total del cromosoma. */
+  protected int tamTotal;
+  
+  /** Precision de la codificacion. */
+  protected double precision;
+  
+  /** Numero total de fenotipos. */
+  protected int numFenotipos;
+
+  /** The rand. */
+  protected Random rand = new Random();
+
+  /**
+   * Constructor vacio.
+   */
+  public Individuo() {
+
+  }
+
+
+  
+  /**
+   * Instantiates a new individuo.
+   *
+   * @param chromosome cromosoma dado
+   */
+  public Individuo(T[] chromosome) {
+    this.chromosome = chromosome;
+  }
+
+  
+  /**
+   * Calcula el fitness de un cromosoma.
+   *
+   * @return fitness de dicho cromosoma
+   */
+  public abstract double fitness();
+
+
+  /**
+   * Muta el cromosoma de un individuo dado.
+   *
+   * @param individuo individuo a mutar
+   * @param prob probabilidad de mutar
+   * @return individuo mutado
+   */
+  public abstract Individuo<T> mutar(Individuo<T> individuo, double prob, int mut);
+
+  /**
+   * Gets the cromosoma.
+   *
+   * @return the cromosoma
+   */
+  public abstract T[] getCromosoma();
+
+  /**
+   * Gets the fenotipo.
+   *
+   * @param index the index
+   * @return the fenotipo
+   */
+  public abstract double getFenotipo(int index);
+
+  /**
+   * Gets the valor.
+   *
+   * @return the valor
+   */
+  public abstract double getValor();
+
+  /**
+   * Tam gen.
+   *
+   * @param valorError precision
+   * @param min the min
+   * @param max the max
+   * @return codificacion de un fenotipo
+   */
+  public int tamGen(double valorError, double min, double max) {
+    return (int) (Math.log10(((max - min) / valorError) + 1) / Math.log10(2));
+  }
+
+  /**
+   * Copy individuo.
+   *
+   * @return nuevo individuo con el mismo cromosoma y precision
+   */
+  public abstract Individuo<T> copyIndividuo();
+
+  /**
+   * Compare to.
+   *
+   * @param i2 otro individuo
+   * @return 0 si son iguales, -1 si i1 tiene un fitness mayor que i2, 1 en caso contrario
+   */
+  public int compareTo(Individuo<T> i2) {
+    if (this.fitness() > i2.fitness()) {
+      return -1;
+    }
+    if (this.fitness() < i2.fitness()) {
+      return 1;
+    }
+    return 0;
+  }
+
+  /**
+   * Comprueba si la funcion es de maximos o de minimos.
+   *
+   * @return true sie s de maximizar
+   */
+  public abstract boolean isMax();
+
+  /**
+   * To string.
+   *
+   * @return the string
+   */
+  public String toString() {
+    String aux = "Cromosoma: ";
+
+    for (int i = 0; i < numFenotipos; i++) {
+      aux += this.getFenotipo(i) + " ";
+    }
+    aux += "\n";
+
+    aux += "fitness: " + this.fitness();
+
+    return aux;
+  }
+  
+  public void corregir() {
+    return;
+  }
+
+}
---
+++ Practicas/G02P3/src/main/java/g02/individuals/IndividuoPractica3.java
@@ -0,0 +1,169 @@
+package g02.individuals;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Individuo de calibrado.
+ */
+public class IndividuoPractica3 extends Individuo<Boolean> {
+
+
+  /**
+   * Instantiates a new individuo practica 3
+   *
+   * @param precision precision de la codificacion
+   */
+  public IndividuoPractica3(double precision) {
+    this.numFenotipos = 2;
+    this.tamGenes = new int[2];
+    this.min = new double[2];
+    this.max = new double[2];
+    this.min[0] = -3.000;
+    this.min[1] = 4.100;
+    this.max[0] = 12.100;
+    this.max[1] = 5.800;
+    this.precision = precision;
+    this.tamGenes[0] = this.tamGen(precision, min[0], max[0]);
+    this.tamGenes[1] = this.tamGen(precision, min[1], max[1]);
+    this.tamTotal = tamGenes[0] + tamGenes[1];
+    this.chromosome = new Boolean[tamTotal];
+    for (int i = 0; i < tamTotal; i++) {
+      this.chromosome[i] = ThreadLocalRandom.current().nextBoolean();
+    }
+
+  }
+
+  /**
+   * Instantiates a new individuo funcion 1.
+   *
+   * @param chromosome cromosoma dado
+   * @param precision precision de la codificacion
+   */
+  public IndividuoPractica3(Boolean[] chromosome, double precision) {
+    this.numFenotipos = 2;
+    this.tamGenes = new int[2];
+    this.min = new double[2];
+    this.max = new double[2];
+    this.min[0] = -3.000;
+    this.min[1] = 4.100;
+    this.max[0] = 12.100;
+    this.max[1] = 5.800;
+    this.precision = precision;
+    this.tamGenes[0] = this.tamGen(precision, min[0], max[0]);
+    this.tamGenes[1] = this.tamGen(precision, min[1], max[1]);
+
+    this.tamTotal = this.tamGenes[0] + this.tamGenes[1];
+    this.chromosome = new Boolean[tamTotal];
+    for (int i = 0; i < tamTotal; i++) {
+      this.chromosome[i] = chromosome[i];
+    }
+  }
+
+
+  /**
+   * Calcula el fitness del individuo.
+   *
+   * @return fitness del individuo
+   */
+  public double fitness() {
+    return this.getValor();
+  }
+
+  /**
+   * Muta un individuo.
+   *
+   * @param individuo Individuo a mutar
+   * @param prob probabilidad de mutacion
+   * @return individuo mutado
+   */
+  @Override
+  public Individuo<Boolean> mutar(Individuo<Boolean> individuo, double prob, int mut) {
+	  for(int i = 0; i < this.tamTotal; i++) {
+		  if (ThreadLocalRandom.current().nextDouble() < prob) {
+
+		      chromosome[i] = !chromosome[i];
+	  }
+    }
+
+    return this;
+  }
+
+  /**
+   * Gets the chromosome.
+   *
+   * @return el cromosoma
+   */
+  @Override
+  public Boolean[] getCromosoma() {
+    return chromosome;
+  }
+
+  /**
+   * Gets the fenotipo of a given chromosome.
+   *
+   * @param index the index of the fenotype
+   * @return the fenotype
+   */
+  // Necesario para calcular el fitness
+  @Override
+  public double getFenotipo(int index) {
+    int start;
+    int end;
+
+    if (index == 0) {
+      start = 0;
+      end = this.tamGenes[index] - 1;
+    } else {
+      start = this.tamGenes[index - 1];
+      end = this.tamGenes[index] + this.tamGenes[index - 1] - 1;
+    }
+
+
+    double res = 0;
+    int power = 0;
+    for (int i = end; i >= start; i--) {
+      if (this.chromosome[i]) {
+        res += Math.pow(2, power);
+      }
+      power++;
+    }
+
+    return this.min[index]
+        + (res * (this.max[index] - this.min[index]) / (Math.pow(2, this.tamGenes[index]) - 1));
+  }
+
+  /**
+   * Valor del individuo en su funcion.
+   *
+   * @return el valor
+   */
+  @Override
+  public double getValor() {
+    double x1 = this.getFenotipo(0);
+    double x2 = this.getFenotipo(1);
+    return (21.5 + x1 * Math.sin(4 * Math.PI * x1) + x2 * Math.sin(20 * Math.PI * x2));
+  }
+
+  /**
+   * Copy individuo.
+   *
+   * @return the individuo
+   */
+  @Override
+  public Individuo<Boolean> copyIndividuo() {
+    return new IndividuoPractica3(this.chromosome, this.precision);
+  }
+
+  /**
+   * Comprueba si es una funcion de maximizacion.
+   *
+   * @return true
+   */
+  @Override
+  public boolean isMax() {
+    return true;
+  }
+
+
+}
